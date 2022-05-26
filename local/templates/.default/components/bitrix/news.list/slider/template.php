337a1f24ab5057843b794db7a9bad56f1326ae6e
DevDIEZTECH <91594404+DevDIEZTECH@users.noreply.github.com>
2022-05-26 18:07:41 +0200

    Проверяй на наличие и сокращай

--- local/templates/.default/components/bitrix/news.list/slider/template.php
+++ local/templates/.default/components/bitrix/news.list/slider/template.php
@@ -12,16 +12,18 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<?foreach($arResult["ITEMS"] as $arItem):?>
+<?foreach($arResult["ITEMS"] as $arItem){?>
 	<div class="swiper-slide" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 		<?
 		$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
 		$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 		?>
 		<div class="reason">
-			<img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="" class="reason__img">
-			<strong class="reason__title"><?echo $arItem["NAME"]?></strong>
-			<small class="reason__text"><?echo $arItem["PREVIEW_TEXT"];?></small>
+			<?if(!empty($arItem["PREVIEW_PICTURE"])){?>
+				<img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="" class="reason__img">
+			<?}?>
+			<strong class="reason__title"><?= $arItem["NAME"]?></strong>
+			<small class="reason__text"><?= $arItem["PREVIEW_TEXT"];?></small>
 		</div>
 	</div>
-<?endforeach;?>
+<?}?>
